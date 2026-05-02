81c5eb214bb9a4ba40bd7431613bcf6ab76972f9
sn9729 <sn9729@srmist.edu.in>
2026-05-02 18:39:44 +0530

    updated files

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template(
         "index.html",
-        version="1.0",
+        version="2.0",
         updated=datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     )
 
